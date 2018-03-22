13a35ab12536d4d1fe92f2c239e1022fbc67f121
Shahar Levy <shaharlevy71@gmail.com>
2018-03-22 13:42:26 -0400

    Added a valid coin acceptance test. Also added a coin and coinbox class.

---
+++ src/test/java/com/ValidCoinSpec.groovy
@@ -0,0 +1,23 @@
+package com
+
+import spock.lang.Specification
+
+
+class ValidCoinSpec extends Specification {
+
+    def "valid coin acceptance"(){
+
+        given: "a coinbox"
+        CoinBox coinbox = new CoinBox();
+        and: "coins of a valid type"
+        Coin coin = new Coin("quarter");
+        and: "initial total of 0"
+        double total = 0;
+        when: "a valid coin is inserted"
+        coinbox.insertCoin(coin);
+        then: "the total value increases by the coin value"
+        coinbox.totalValue == coin.getCoinValue()
+
+    }
+
+}
