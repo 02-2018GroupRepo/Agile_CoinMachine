79c940a95b673b3cd45d1efeb358ba954b22a906
Jrod9599 <Jrod9599@gmail.com>
2018-03-23 14:19:21 -0400

    fixed errors

--- src/main/java/com/Vending_Machine.java
+++ src/main/java/com/Vending_Machine.java
@@ -16,13 +16,12 @@ public class Vending_Machine {
 //    int numN = 50; //Number of Nickels, $2.50
 //    int numD = 50; //Number of Dimes $5.00
 
-    Map<Products, Integer> inStock = new HashMap<Products, Integer>();
     CoinBox box = new CoinBox();
     Queue<Products>[][] products2DArray = new Queue[5][5];
 
     public Vending_Machine(){
         initializeItemQueues();
-    };
+    }
 
     public Vending_Machine(int ID, String location){
         this.location = location;
@@ -46,36 +45,8 @@ public class Vending_Machine {
         this.company = company;
     }
 
-    public void showMenu(){
-        ArrayList<String> productSort = new ArrayList<String>();
 
-        for (Map.Entry<Products, Integer> entry : inStock.entrySet()) {
-            productSort.add(entry.getKey().getId() + " " + entry.getKey().getName() + " $" + entry.getKey().getRetailPrice());
-        }
-
-        Collections.sort(productSort);
-
-        for(String i: productSort) {
-            System.out.println(i);
-        }
-    }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public boolean coinReturn(double price, double moneyGiven){
-        boolean answer = false;
-        if(moneyGiven >= price){
-            answer = true;
-            return answer;
-        }
-        else{
-            return answer;
-        }
-=======
-=======
-
->>>>>>> 513ab7433d72e806f4216378cd3d003289c7a6b8
-    public void initializeItemQueues (){
+    public void initializeItemQueues(){
         for (int i = 0; i < products2DArray.length; i++){
             for (int j = 0; j < products2DArray[i].length; j++){
                 products2DArray[i][j] = new ArrayDeque<Products>();
@@ -85,15 +56,12 @@ public class Vending_Machine {
 
 
     public boolean isInStock (int column, int row){
-    boolean inStock = true;
+        boolean inStock = true;
         if(products2DArray[column][row].size() == 0){
              inStock = false;
             System.out.println("Out of stock, idiot.");
         }
-    return inStock;
-<<<<<<< HEAD
->>>>>>> 6b35bb72ff38ca247e76a3626351512b1b0d4c91
-=======
+         return inStock;
     }
 
     public boolean coinReturn(double price, double moneyGiven){
@@ -106,7 +74,6 @@ public class Vending_Machine {
             return answer;
         }
 
->>>>>>> 513ab7433d72e806f4216378cd3d003289c7a6b8
     }
 
 }
