c3c11d51ad19ea1e598685e3901f7bbb7f0e0350
Shahar Levy <shaharlevy71@gmail.com>
2018-03-23 13:58:00 -0400

    Added a test and functionality to check if an item is out of stock.

--- src/main/java/com/Vending_Machine.java
+++ src/main/java/com/Vending_Machine.java
@@ -2,10 +2,11 @@ package com;
 
 import java.util.*;
 
+
 public class Vending_Machine {
 
     String location;
-    String company = "Shane";
+    String company = "Shame";
     int ID = 0;
 
     int MAX_ITEM_COUNT;
@@ -13,16 +14,20 @@ public class Vending_Machine {
 //    //Starting each vending machine with $20
 //    int numQ = 50; //Number of Quarters, $12.50
 //    int numN = 50; //Number of Nickels, $2.50
-//    int numD = 50; //Number of Dimesm $5.00
+//    int numD = 50; //Number of Dimes $5.00
 
     Map<Products, Integer> inStock = new HashMap<Products, Integer>();
     CoinBox box = new CoinBox();
+    Queue<Products>[][] products2DArray = new Queue[5][5];
 
-    public Vending_Machine(){};
+    public Vending_Machine(){
+        initializeItemQueues();
+    };
 
     public Vending_Machine(int ID, String location){
         this.location = location;
         this.ID = ID;
+        initializeItemQueues();
     }
 
     public String getLocation(){
@@ -42,17 +47,35 @@ public class Vending_Machine {
     }
 
     public void showMenu(){
-        ArrayList<String> candySort = new ArrayList<String>();
+        ArrayList<String> productSort = new ArrayList<String>();
 
         for (Map.Entry<Products, Integer> entry : inStock.entrySet()) {
-            candySort.add(entry.getKey().getId() + " " + entry.getKey().getName() + " $" + entry.getKey().getRetailPrice());
+            productSort.add(entry.getKey().getId() + " " + entry.getKey().getName() + " $" + entry.getKey().getRetailPrice());
         }
 
-        Collections.sort(candySort);
+        Collections.sort(productSort);
 
-        for(String i: candySort) {
+        for(String i: productSort) {
             System.out.println(i);
         }
+    }
+
+    public void initializeItemQueues (){
+        for (int i = 0; i < products2DArray.length; i++){
+            for (int j = 0; j < products2DArray[i].length; j++){
+                products2DArray[i][j] = new ArrayDeque<Products>();
+            }
+        }
+    }
+
+
+    public boolean isInStock (int column, int row){
+    boolean inStock = true;
+        if(products2DArray[column][row].size() == 0){
+             inStock = false;
+            System.out.println("Out of stock, idiot.");
+        }
+    return inStock;
     }
 
 }
